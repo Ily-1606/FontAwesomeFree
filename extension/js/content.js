ba4adad8b3a16d2a1285159318b06480fe4daf44
ILY-1606 <no1.ily1606@gmail.com>
2022-03-06 20:26:36 +0700

    fix extension

--- extension/js/content.js
+++ extension/js/content.js
@@ -4,6 +4,11 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       const searchParams = new URLSearchParams(search);
       const style = searchParams.get('s');
       const iconName = window.location.pathname.split('icons/')[1]
-      window.open(`https://site-assets.fontawesome.com/releases/v6.0.0/svgs/${style}/${iconName}.svg`)
+      if(style && iconName){
+        window.open(`https://site-assets.fontawesome.com/releases/v6.0.0/svgs/${style}/${iconName}.svg`)
+      }
+      else{
+        alert("Not found icon")
+      }
   }
 });
